4ee48587fa345d5a4b1ab8bccf1580136e90d5b9
fab19n <54259425+fab19n@users.noreply.github.com>
2026-06-26 02:14:06 +0800

    configured vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Newsreader, DM_Sans, Space_Mono } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const newsreader = Newsreader({
   subsets: ['latin'],
@@ -33,14 +35,14 @@ export const metadata: Metadata = {
   },
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode}) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={`${newsreader.variable} ${dmSans.variable} ${spaceMono.variable}`}
-    >
-      <body>{children}</body>
+    <html>
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
